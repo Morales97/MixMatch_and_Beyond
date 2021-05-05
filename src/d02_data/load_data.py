b720a5472ef7f588260e7153cc9312f9bfbf30c5
Morales97 <danimoralesbrotons@gmail.com>
2021-05-05 11:38:33 +0200

    validation dataloader

--- src/d02_data/load_data.py
+++ src/d02_data/load_data.py
@@ -1,14 +1,17 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+
 
 def get_dataloaders(path='../../data', batch_size=64):
-
     transform_train = transforms.Compose(
         # transforms.RandomCrop(32, padding=4),
         # transforms.RandomHorizontalFlip(),
         [transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
@@ -17,20 +20,83 @@ def get_dataloaders(path='../../data', batch_size=64):
 
     batch_size = batch_size
 
-    trainset = torchvision.datasets.CIFAR10(root=path, train=True,
-                                            download=True, transform=transform_train)
+    trainset = datasets.CIFAR10(root=path, train=True,
+                                download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root=path, train=False,
-                                        download=True, transform=transform_test)
+    testset = datasets.CIFAR10(root=path, train=False,
+                               download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     return trainset, trainloader, testset, testloader
+
+
+def get_dataloaders_validation(path="../../data", batch_size=64, shuffle=False, augment=False,
+                               train_size=45000, val_size=5000):
+    """
+    Include split in train and valdiation set
+    """
+    assert ((train_size >= 0) and (val_size >= 0) and (train_size + val_size <= 50000))
+
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    if augment:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])
+    else:
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    # Need to load dataset twice in case train data is augmented but not validation
+    train_set = datasets.CIFAR10(root=path, train=True,
+                                 download=True, transform=transform_train)
+
+    val_set = datasets.CIFAR10(root=path, train=True,
+                               download=True, transform=transform_val)
+
+    test_set = datasets.CIFAR10(root=path, train=False,
+                                download=True, transform=transform_test)
+
+    indices = list(range(train_size + val_size))
+    if shuffle:
+        np.random.seed(1)  # Hardcoded seed for the moment
+        np.random.shuffle(indices)
+    train_idx, valid_idx = indices[:train_size], indices[train_size:]
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               sampler=train_sampler, num_workers=2)
+    valid_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
+                                               sampler=valid_sampler, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                              shuffle=False, num_workers=2)
+
+    return train_loader, valid_loader, test_loader
+
 
 if __name__ == "__main__":
     trainset, trainloader, testset, testloader = get_dataloaders()
--- src/main.py
+++ src/main.py
@@ -4,17 +4,22 @@ el main 8==D
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import pdb
 
-from d02_data.load_data import get_dataloaders
+from d02_data.load_data import get_dataloaders, get_dataloaders_validation
 from d04_mixmatch.vanilla_net import VanillaNet
 from d07_visualization.visualize_cifar10 import show_img
+
+
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # Assuming that we are on a CUDA machine, this should print a CUDA device:
     print(device)
 
-    trainset, trainloader, testset, testloader = get_dataloaders(path='../data')
+    # trainset, train_loader, testset, test_loader = get_dataloaders(path='../data')
+    train_loader, val_loader, test_loader = get_dataloaders_validation(path='../data', batch_size=16)
     net = VanillaNet()
     net.to(device)
 
@@ -23,10 +28,14 @@ if __name__ == '__main__':
 
     # show_img(iter(trainloader).next()[0][0])
 
+    n_batches_train = np.floor(train_loader.sampler.__len__() / train_loader.batch_size)
+    n_batches_val = np.floor(val_loader.sampler.__len__() / val_loader.batch_size)
+    loss_train, loss_val = [], []
+
     for epoch in range(2):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(train_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
             # inputs, labels = data
             inputs, labels = data[0].to(device), data[1].to(device)  # send to cuda
@@ -41,19 +50,42 @@ if __name__ == '__main__':
             optimizer.step()
 
             # print statistics
+            """
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
+            if i % 300 == 99:    # print every 300 mini-batches
+                print('[Epoch %d, Step %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 300))
                 running_loss = 0.0
+            """
+
+        # Compute train and validation loss
+        with torch.no_grad():
+            train_loss = 0
+            for i, data in enumerate(train_loader, 0):
+                inputs, labels = data[0].to(device), data[1].to(device)
+                outputs = net(inputs)
+                train_loss += criterion(outputs, labels).item()
+            train_loss /= n_batches_train
+            loss_train.append(train_loss)
+            val_loss = 0
+            for i, data in enumerate(val_loader, 0):
+                inputs, labels = data[0].to(device), data[1].to(device)
+                outputs = net(inputs)
+                val_loss += criterion(outputs, labels).item()
+            val_loss /= n_batches_val
+            loss_val.append(val_loss)
+
+        print("Epoch %d done.\t Train loss: %.3f \t Validation loss: %.3f" %
+              (epoch+1, train_loss, val_loss))
 
     print('Finished Training')
 
-    # calculate accuracy
+
+    # calculate accuracy on test set
     correct, total = 0, 0
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
-        for data in testloader:
+        for data in test_loader:
             images, labels = data
             # calculate outputs by running images through the network
             outputs = net(images)
@@ -64,3 +96,6 @@ if __name__ == '__main__':
 
     print('Accuracy of the network on the 10000 test images: %d %%' % (
             100 * correct / total))
+
+
+
