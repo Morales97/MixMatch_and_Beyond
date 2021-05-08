5a6b1b1ccede09548fa1b6b256524bb9e3a6bcb0
DamianValle <damianvs@kth.se>
2021-05-08 18:40:09 +0200

    data augmentation implemented

---
+++ src/d03_processing/transform_data.py
@@ -0,0 +1,26 @@
+import torchvision.transforms as transforms
+import torch
+
+class TransformTwice:
+    def __init__(self, K=2):
+        self.K = K
+        self.transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            transforms.RandomAffine(degrees = 0, translate = (0.1, 0.1)),
+                                            AddGaussianNoise(mean=0, std=0.15)])
+
+    def __call__(self, batch):
+        augmented_batch = []
+        for _ in range(self.K):
+            augmented_batch.append(self.transform(batch))
+        return augmented_batch
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
