97fdaee4c3d9fe453ac1ba036e37593f5b0458c2
DamianValle <damianvs@kth.se>
2021-05-08 18:42:59 +0200

    cleanup

--- src/d07_visualization/visualize_transforms.py
+++ src/d07_visualization/visualize_transforms.py
@@ -5,6 +5,8 @@ from d02_data.load_data import get_dataloaders, get_dataloaders_validation
 from d03_processing.transform_data import TransformTwice
 from d07_visualization.visualize_cifar10 import show_img, show_grid
 
+# Mover un directorio parriba pa que chute
+
 if __name__ == '__main__':
     augment_unlabeled = TransformTwice(K=4)
     train_loader, val_loader, test_loader = get_dataloaders_validation(path='../data', batch_size=1)
