e86233e9d93a098dcd924aac9c06e1bc380708bb
Morales97 <danimoralesbrotons@gmail.com>
2021-05-10 17:16:18 +0200

    init weights

--- src/d04_mixmatch/wideresnet.py
+++ src/d04_mixmatch/wideresnet.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
 from torchsummary import summary
-
+import math
 
 class BasicBlock(nn.Module):
 
     def __init__(self, n_features):
         super(BasicBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(n_features)
+        self.bn1 = nn.BatchNorm2d(n_features, momentum=0.001)
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.conv1 = nn.Conv2d(n_features, n_features, kernel_size=(3, 3), padding=1)
 
-        self.bn2 = nn.BatchNorm2d(n_features)
+        self.bn2 = nn.BatchNorm2d(n_features, momentum=0.001)
         self.conv2 = nn.Conv2d(n_features, n_features, kernel_size=(3, 3), padding=1)
 
     def forward(self, x):
@@ -40,11 +40,11 @@ class TransitionBlock(nn.Module):
             stride = 1
 
         super(TransitionBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_f)
+        self.bn1 = nn.BatchNorm2d(in_f, momentum=0.001)
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.conv1 = nn.Conv2d(in_f, out_f, kernel_size=(3, 3), padding=1, stride=stride)
 
-        self.bn2 = nn.BatchNorm2d(out_f)
+        self.bn2 = nn.BatchNorm2d(out_f, momentum=0.001)
         self.conv2 = nn.Conv2d(out_f, out_f, kernel_size=(3, 3), padding=1)
 
         # Shortcut connection for identity to match dimensions
@@ -87,10 +87,22 @@ class WideResNet(nn.Module):
         self.conv_group1 = ConvGroup(n_features[0], n_features[1], blocks=n, downsample=False)
         self.conv_group2 = ConvGroup(n_features[1], n_features[2], blocks=n)
         self.conv_group3 = ConvGroup(n_features[2], n_features[3], blocks=n)
-        self.bn = nn.BatchNorm2d(n_features[3])
+        self.bn = nn.BatchNorm2d(n_features[3], momentum=0.001)
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.avg_pool = nn.AvgPool2d(kernel_size=8)
         self.linear = nn.Linear(n_features[3], n_out)
+
+        # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.zero_()
 
     def forward(self, x):
         x = self.conv1(x)
