fa0d3053319ae3d125f316ec39f1a7c9277f521d
Morales97 <danimoralesbrotons@gmail.com>
2021-05-13 12:45:13 +0200

    tamos casi ready 2

--- src/main.py
+++ src/main.py
@@ -5,99 +5,79 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import pdb
 
-from d02_data.load_data import get_dataloaders, get_dataloaders_validation
-from d03_processing.transform_data import TransformTwice
-from d04_mixmatch.vanilla_net import VanillaNet
-from d07_visualization.visualize_cifar10 import show_img
+from d02_data.load_data import get_dataloaders_ssl
+from d04_mixmatch.wideresnet import WideResNet
+from mixmatch import MixMatch
 
 if __name__ == '__main__':
+
+    batch_size = 64
+    num_labeled = 250
+    n_steps = 10
+    K = 2
+    lambda_u = 75
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # Assuming that we are on a CUDA machine, this should print a CUDA device:
-    print(device)
 
-    augment_labeled = TransformTwice(K=1)
-    augment_unlabeled = TransformTwice(K=2)
+    labeled_loader, unlabeled_loader, val_loader, test_loader = get_dataloaders_ssl(path='../data',
+                                                                                    batch_size=batch_size,
+                                                                                    num_labeled=num_labeled)
+    model = WideResNet(depth=28, k=2, n_out=10)
+    model.to(device)
 
-    # trainset, train_loader, testset, test_loader = get_dataloaders(path='../data')
-    train_loader, val_loader, test_loader = get_dataloaders_validation(path='../data', batch_size=16)
-    net = VanillaNet()
-    net.to(device)
+    criterion_x = nn.CrossEntropyLoss()
+    criterion_u = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.002)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    iter_labeled_loader = iter(labeled_loader)
+    iter_unlabeled_loader = iter(unlabeled_loader)
 
-    # show_img(iter(trainloader).next()[0][0])
+    for step in range(n_steps):
 
-    n_batches_train = np.floor(train_loader.sampler.__len__() / train_loader.batch_size)
-    n_batches_val = np.floor(val_loader.sampler.__len__() / val_loader.batch_size)
-    loss_train, loss_val = [], []
+        # try-catch of dataloaders
+        try:
+            x_imgs, x_labels = iter_labeled_loader.next()
+            # Check if batch size has been cropped for last batch
+            if x_imgs.shape[0] < batch_size:
+                iter_labeled_loader = iter(labeled_loader)
+                x_imgs, x_labels = iter_labeled_loader.next()
+        except:
+            iter_loader = iter(labeled_loader)
+            x_imgs, x_labels = iter_loader.next()
 
-    for epoch in range(2):  # loop over the dataset multiple times
+        try:
+            u_imgs, _ = iter_unlabeled_loader.next()
+            if u_imgs.shape[0] < batch_size:
+                iter_unlabeled_loader = iter(unlabeled_loader)
+                u_imgs, _ = iter_unlabeled_loader.next()
+        except:
+            iter_unlabeled_loader = iter(unlabeled_loader)
+            u_imgs, _ = iter_unlabeled_loader.next()
 
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            # inputs, labels = data
-            inputs, labels = data[0].to(device), data[1].to(device)  # send to cuda
+        mixmatch = MixMatch(model, batch_size)
+        x, u = mixmatch.run(x_imgs, x_labels, u_imgs)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        x_input = x[0]
+        x_targets = x[1]
+        u_input = u[0]
+        u_targets = u[1].detach_()  # stop gradients from propagation to label guessing. Is this necessary?
 
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+        # Compute X' predictions
+        x_output = model(x_input)
 
-            # print statistics
-            """
-            running_loss += loss.item()
-            if i % 300 == 99:    # print every 300 mini-batches
-                print('[Epoch %d, Step %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 300))
-                running_loss = 0.0
-            """
+        # Compute U' predictions
+        # Separate in batches (does it make any difference? maybe on batchnorm. Original implementation does it)
+        u_batch_outs = []
+        for k in range(K):
+            u_batch = u_input[k * batch_size:(k + 1) * batch_size]
+            u_batch_outs.append(model(u_batch))
+        u_outputs = torch.cat(u_batch_outs, dim=0)
 
-        # Compute train and validation loss
-        with torch.no_grad():
-            train_loss = 0
-            for i, data in enumerate(train_loader, 0):
-                inputs, labels = data[0].to(device), data[1].to(device)
-                outputs = net(inputs)
-                train_loss += criterion(outputs, labels).item()
-            train_loss /= n_batches_train
-            loss_train.append(train_loss)
-            val_loss = 0
-            for i, data in enumerate(val_loader, 0):
-                inputs, labels = data[0].to(device), data[1].to(device)
-                outputs = net(inputs)
-                val_loss += criterion(outputs, labels).item()
-            val_loss /= n_batches_val
-            loss_val.append(val_loss)
+        # Compute loss
+        lx = criterion_x(x_output, x_targets)
+        lu = criterion_u(u_outputs, u_targets)
+        loss = lx + lambda_u * lu  # TODO ramp up lambda_u
 
-        print("Epoch %d done.\t Train loss: %.3f \t Validation loss: %.3f" %
-              (epoch+1, train_loss, val_loss))
-
-    print('Finished Training')
-
-
-    # calculate accuracy on test set
-    correct, total = 0, 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
-    with torch.no_grad():
-        for data in test_loader:
-            images, labels = data
-            # calculate outputs by running images through the network
-            outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-            100 * correct / total))
-
-
-
+        # Step
+        loss.backward()
+        optimizer.step()
