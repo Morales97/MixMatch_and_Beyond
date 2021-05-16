15ec41bf3a9768d6953133f51c06f711baf37c7e
Morales97 <danimoralesbrotons@gmail.com>
2021-05-16 16:58:57 +0200

    rm BN momentum

--- src/d04_mixmatch/wideresnet_no_bias.py
+++ src/d04_mixmatch/wideresnet_no_bias.py
@@ -3,15 +3,16 @@ import torch.nn as nn
 from torchsummary import summary
 import math
 
+
 class BasicBlock(nn.Module):
 
     def __init__(self, n_features):
         super(BasicBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(n_features, momentum=0.001)   # when trying EMA with many epochs, try using BN with momentum=0.001
+        self.bn1 = nn.BatchNorm2d(n_features)   # when trying EMA with many epochs, try using BN with momentum=0.001
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.conv1 = nn.Conv2d(n_features, n_features, kernel_size=(3, 3), padding=1, bias=False)
 
-        self.bn2 = nn.BatchNorm2d(n_features, momentum=0.001)
+        self.bn2 = nn.BatchNorm2d(n_features)
         self.conv2 = nn.Conv2d(n_features, n_features, kernel_size=(3, 3), padding=1, bias=False)
 
     def forward(self, x):
@@ -40,11 +41,11 @@ class TransitionBlock(nn.Module):
             stride = 1
 
         super(TransitionBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_f, momentum=0.001)
+        self.bn1 = nn.BatchNorm2d(in_f)
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.conv1 = nn.Conv2d(in_f, out_f, kernel_size=(3, 3), padding=1, stride=stride, bias=False)
 
-        self.bn2 = nn.BatchNorm2d(out_f, momentum=0.001)
+        self.bn2 = nn.BatchNorm2d(out_f)
         self.conv2 = nn.Conv2d(out_f, out_f, kernel_size=(3, 3), padding=1, bias=False)
 
         # Shortcut connection for identity to match dimensions
@@ -87,7 +88,7 @@ class WideResNetUnbias(nn.Module):
         self.conv_group1 = ConvGroup(n_features[0], n_features[1], blocks=n, downsample=False)
         self.conv_group2 = ConvGroup(n_features[1], n_features[2], blocks=n)
         self.conv_group3 = ConvGroup(n_features[2], n_features[3], blocks=n)
-        self.bn = nn.BatchNorm2d(n_features[3], momentum=0.001)
+        self.bn = nn.BatchNorm2d(n_features[3])
         self.relu = nn.LeakyReLU(negative_slope=0.1)
         self.avg_pool = nn.AvgPool2d(kernel_size=8)
         self.linear = nn.Linear(n_features[3], n_out)
@@ -121,7 +122,7 @@ class WideResNetUnbias(nn.Module):
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = WideResNet(depth=28, k=2, n_out=10)
+    model = WideResNetUnbias(depth=28, k=2, n_out=10)
     model.to(device)
     summary(model, (3, 32, 32))
 
