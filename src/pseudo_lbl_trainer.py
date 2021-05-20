5808d073300be1c0eaf4de31dc56406c0b7a05f3
Morales97 <danimoralesbrotons@gmail.com>
2021-05-20 09:37:44 +0200

    pseudo

--- src/pseudo_lbl_trainer.py
+++ src/pseudo_lbl_trainer.py
@@ -161,7 +161,7 @@ class PseudoLabelTrainer:
                     correct = torch.sum(pseudo_labels[:, 4]).item()
                     print('Confidence threshold %.3f\t Generated / Correct / Precision\t %d\t%d\t%.2f '
                           % (tau, total, correct, correct / (total + np.finfo(float).eps) * 100))
-
+                
                 unlbl_mask1 = (matrix[:, 1] < self.tau)
                 #unlbl_mask2 = (matrix[:, 1] >= 0.99)
                 pseudo_mask = (matrix[:, 1] >= self.tau)
